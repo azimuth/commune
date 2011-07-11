e2c4697e8746beffc67b075a1fe392e37f08810d
Christopher Maujean <cmaujean@gmail.com>
2011-07-10 23:31:14 -0700

    enhance vendor specs

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -9,13 +9,16 @@ describe Vendor do
     @v.name.should eq "Foo, Inc"
   end
   
-  it "has an array at #expense_reports" do
-    @v.expense_reports.should eq []
-  end
-  
   it "has_many expense_reports" do
     ep = mock_model(ExpenseReport)
     @v.expense_reports << ep
+    @v.should be_valid
     @v.expense_reports.should eq [ep]
+  end
+  
+  it "doesn't have to have any expense_reports" do
+    vv = Vendor.create(:name => "Foo, Inc too")
+    vv.should be_valid
+    vv.expense_reports.should eq []
   end
 end
